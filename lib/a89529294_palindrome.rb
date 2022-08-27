30c83e5ef13138117b06b218e2283b07b1947b88
Albert Chang <a89529294@hotmail.com>
2022-08-27 16:29:50 +0800

    Add palindrome method to Integer

--- lib/a89529294_palindrome.rb
+++ lib/a89529294_palindrome.rb
@@ -1,15 +1,24 @@
 # frozen_string_literal: true
 
 require_relative "a89529294_palindrome/version"
-
-class String
+module A89529294_palindrome
+  # Returns true for a palindrome, false otherwise.
   def palindrome?
     processed_content == processed_content.reverse
   end
 
   private
 
+  # Returns content for palindrome testing.
   def processed_content
-    scan(/[a-z]/i).join.downcase
+    to_s.scan(/\w/).join.downcase
   end
+end
+
+class String
+  include A89529294_palindrome
+end
+
+class Integer
+  include A89529294_palindrome
 end
--- test/test_a89529294_palindrome.rb
+++ test/test_a89529294_palindrome.rb
@@ -18,4 +18,12 @@ class TestA89529294Palindrome < Minitest::Test
   def test_palindrome_with_punctuation
     assert %(Madam, I’m Adam.).palindrome?
   end
+
+  def test_integer_non_palindrome
+    refute 12_345.palindrome?
+  end
+
+  def test_integer_palindrome
+    assert 12_321.palindrome?
+  end
 end
